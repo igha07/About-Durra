b9c4f9c8341cd98a1aa66c058b594fd151a8642f
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-12-28 16:19:34 +0300

    deployment updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/About-Durra',
-  outDir: 'docs',        // build into docs/ so GitHub Pages can serve it
-  output: 'static',      // this is the default, but it's nice to be explicit
+  outDir: 'docs',
+  output: 'static',
 });
